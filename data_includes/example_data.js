d6c89b8687de6902ff69730bbb64765eeed0f88a
dmacpalle <52783923+dmacpalle@users.noreply.github.com>
2020-03-26 14:20:41 +0100

    Add files via upload

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,19 +1,35 @@
-// We only generate trials labeled 'rating' so far (from the Label column)
-var shuffleSequence = seq("rating");
-PennController.ResetPrefix(null);
-
-// PennController.FeedItems automatically creates the 'items' variable for you
-PennController.FeedItems(
-    // 'row' will successively point to each row of the table (feel free to use another name)
-    row => PennController(
-        // row.Sentence will iteratively take the value of the column 'Sentence' for each row
-        newText( "green" , row.Sentence )
-        ,
-        newScale("judgment",    "cold", "cool", "lukewarm", "warm", "hot")
-            .settings.labels("top")
-            .settings.before( getText("green") )
-            .settings.size("auto")
-            .print()
-            .wait()   
-    )
-);
+    PennController.Sequence( randomize("picture") , randomize("rating") );
+    PennController.ResetPrefix(null);
+    PennController.AddHost("http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/");
+    PennController.Template( PennController.defaultTable.filter("Type","rating") ,
+        row => PennController( "rating" ,
+            newText( "sentence" , row.Sentence )
+            ,
+            newScale("judgment",    "cold", "cool", "lukewarm", "warm", "hot")
+                .settings.log()             // Record
+                .settings.labelsPosition("top")
+                .settings.before( getText("sentence") )
+                .settings.size("auto")
+                .print()
+                .wait()
+        )
+    );
+    PennController.Template( PennController.defaultTable.filter("Type","picture") ,
+        row => PennController( "picture" ,
+            defaultImage
+                .settings.size(200, 200)
+            ,
+            newText("test sentence", row.Sentence)
+                .print()
+            ,
+            newCanvas("patches", 500, 200)
+                .settings.add(   0, 0, newImage("color1", row.Color1) )
+                .settings.add( 300, 0, newImage("color2", row.Color2) )
+                .print()
+            ,
+            newSelector("patch")
+                .settings.log()             // Record
+                .settings.add( getImage("color1") , getImage("color2") )
+                .wait()
+        )
+    );
--- data_includes/main.js
+++ data_includes/main.js
@@ -1,30 +1,40 @@
+// Show the picture trials first (though we generate them second)
+PennController.Sequence( randomize("picture") , randomize("rating") );
 PennController.ResetPrefix(null);
-
-// Picture selection trial
-PennController(
-    defaultImage
-        .settings.size(200, 200)
-    ,
-    newText("test sentence", "Which patch is greener?")
-        .print()
-    ,
-    newCanvas("patches", 500, 200)
-        .settings.add(   0, 0, newImage("color1", "green1.png") )   // Embedded image creation
-        .settings.add( 300, 0, newImage("color2", "green2.png") )   // Embedded image creation
-        .print()
-    ,
-    newSelector("patch")
-        .settings.add( getImage("color1") , getImage("color2") )
-        .wait()
+// We don't give the full URL in the spreadsheet
+PennController.AddHost("http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/");
+// As before, we use Template to define a template
+// But now, we also specify that we want to use a subset of the table:
+// this template only uses the rows where Type is 'rating'
+PennController.Template( PennController.defaultTable.filter("Type","rating") ,
+    row => PennController( "rating" ,
+        newText( "sentence" , row.Sentence )
+        ,
+        newScale("judgment",    "cold", "cool", "lukewarm", "warm", "hot")
+            .settings.labelsPosition("top")
+            .settings.before( getText("sentence") )
+            .settings.size("auto")
+            .print()
+            .wait()
+    )
 );
-
-PennController(
-    newText("green", "To me the color green is...")
-    ,
-    newScale("judgment",    "cold", "cool", "lukewarm", "warm", "hot")
-        .settings.labelsPosition("top")
-        .settings.before( getText("green") )
-        .settings.size("auto")  // Use max width for each cell (here, lukewarm's cell's width)
-        .print()
-        .wait()
+// We use Template a second time to define a template for the 'picture' trials
+// This template only uses the rows where Type is 'picture'
+PennController.Template( PennController.defaultTable.filter("Type","picture") ,
+    row => PennController( "picture" ,
+        defaultImage
+            .settings.size(200, 200)
+        ,
+        newText("test sentence", row.Sentence)
+            .print()
+        ,
+        newCanvas("patches", 500, 200)
+            .settings.add(   0, 0, newImage("color1", row.Color1) )
+            .settings.add( 300, 0, newImage("color2", row.Color2) )
+            .print()
+        ,
+        newSelector("patch")
+            .settings.add( getImage("color1") , getImage("color2") )
+            .wait()
+    )
 );
